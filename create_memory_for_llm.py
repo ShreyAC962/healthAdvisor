04d02b1ac4316d84850684bb43536c312b017504
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-25 17:30:34 -0700

    feat: split loaded PDFs into 500-token overlapping text chunks

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -9,6 +9,14 @@ def load_pdf_files(data):
     return documents
 documents = load_pdf_files(data=DATA_PATH)
 print("Length of PDF Pages: ", len(documents))
+
 # Create Chunks
+def create_chunks(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 50)
+    text_chunks = text_splitter.split_documents(extracted_data)
+    return text_chunks
+text_chunks = create_chunks(extracted_data=documents)
+print("Length of Text chunks: ", len(text_chunks))
+
 # Create Vector Embeddings
 # Store Embeddings in FAISS
