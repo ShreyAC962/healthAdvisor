7868f4ab2d84450cb5caff219f9a95a2ab44302b
ShreyAC962 <97959780+ShreyAC962@users.noreply.github.com>
2026-03-26 17:24:47 -0700

    fix: initialize HuggingFace embeddings correctly and store document vectors in FAISS index

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -1,6 +1,8 @@
 # Load raw PDF(s)
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 
 DATA_PATH = "data/"
 def load_pdf_files(data):
@@ -8,7 +10,7 @@ def load_pdf_files(data):
     documents = loader.load()
     return documents
 documents = load_pdf_files(data=DATA_PATH)
-print("Length of PDF Pages: ", len(documents))
+# print("Length of PDF Pages: ", len(documents))
 
 # Create Chunks
 def create_chunks(extracted_data):
@@ -16,7 +18,15 @@ def create_chunks(extracted_data):
     text_chunks = text_splitter.split_documents(extracted_data)
     return text_chunks
 text_chunks = create_chunks(extracted_data=documents)
-print("Length of Text chunks: ", len(text_chunks))
+# print("Length of Text chunks: ", len(text_chunks))
 
 # Create Vector Embeddings
+def get_embedding_model():
+    embedding_model = HuggingFaceEmbeddings(model_name="sentecne-transformers/all-MiniLM-L6-V2")
+    return get_embedding_model
+emedding_model= get_embedding_model()
+
 # Store Embeddings in FAISS
+DB_FAISS_PATH="vectorstore/db_faiss"
+db = FAISS.from_documents(text_chunks, emedding_model)
+db.save_local(DB_FAISS_PATH)
